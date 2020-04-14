1a5a314aaa67b390ad12f9d19fbdc9ebbfdab900
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-04-14 17:22:47 -0300

    Prova 01 de Linguagem de Programação

---
+++ Prova 01 de Linguagem de Programação.py
@@ -0,0 +1,60 @@
+#Prova 01 de Linguagem de Programação
+
+#Questão 01
+'''
+a = int(input())
+b = int(input())
+print(a+b)
+'''
+
+#Questão 02
+'''
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+media = (a + b + c + d)/4
+print('A media das alturas eh:',media)
+'''
+
+#Questão 03
+'''
+a = float(input())
+b = 25.4*a
+print(a,'polegada(s) eh o mesmo que',b,'mm')
+'''
+
+#Questão 04 - está errado
+'''
+f = float(input())
+c = (f-32)*5/9
+k = c+273.15
+print('Convertendo', f, '°F temos:\n' + str(c), '°C', 'e', k, 'K')
+'''
+
+#Questão 05
+'''
+# O codigo para a entrada de dados e impressao
+# dos resultados eh dado.
+v1 = input()
+v2 = input()
+
+# insira na janela abaixo o codigo responsavel por
+# trocar os valores de uma variavel para a outra
+troca = v1
+v1 = v2
+v2 = troca
+
+print('valor em v1:', v1)
+print('valor em v2:', v2)
+'''
+
+#Questão 06
+'''
+l = float(input())
+a = l**2*3**0.5/4*6
+p = 6*l
+print('Lado do hexagono:', l, 'metros,')
+print('Area:', a, 'metros quadrados,')
+print('Perimetro:', p, 'metros.')
+'''
